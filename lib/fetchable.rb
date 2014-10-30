169e2a5433fc5c7ccacba74bd26b4e801f952965
Lasse Bunk <lasse@bunk.io>
2014-10-30 12:08:44 +0100

    Update documentation for Fetchable

--- lib/fetchable.rb
+++ lib/fetchable.rb
@@ -1,38 +1,39 @@
-# Module to be included in objects that can be fetched, e.g. a User, Product, etc.
+# When included, marks a class as being fetchable, e.g. a User, Product, etc.
 module Fetchable
   def self.included(base)
     base.extend ClassMethods
   end
 
-  # Returns an instance of the fetch handler defined with +fetches_with+.
-  # If +fetches_with+ isn't set manually, it will derive the fetch module
-  # from its name, e.g. a `Product` uses `ProductFetch`.
+  # Returns an instance of the fetcher defined with +fetches_with+.
+  # If +fetches_with+ isn't set manually, it will derive the fetcher class
+  # from the class name, e.g. a `Product` uses `ProductFetcher` by default.
   #
   #   class Product < ActiveRecord::Base
   #     include Fetchable
-  #     fetches_with "MyFetch"
+  #     fetches_with "MyFetcher"
   #   end
   #
-  #   Product.find(2).fetcher # => instance of MyFetch
+  #   Product.find(2).fetcher # => instance of MyFetcher
   def fetcher
     @fetcher ||= self.class.fetcher.new(self)
   end
 
   module ClassMethods
-    # Defines which fetch handler to use when fetching instances of this class.
+    # Defines which fetcher to use when fetching instances of this class.
     # Can be a class or string, which will be instantiated and returned from
-    # +#fetch+.
+    # +#fetcher+.
     def fetches_with(class_name)
       @fetcher = class_name
     end
 
-    # Returns the fetch handler class set with +fetches_with+.
-    # Default is derived from the name of the class, e.g.:
+    # Returns the fetcher class set with +fetches_with+.
+    # If no fetcher class has been set, it will derive it from the class name,
+    # e.g.:
     #
     #   class Product < ActiveRecord::Base
     #   end
     #   
-    #   Product.fetcher # => ProductFetch
+    #   Product.fetcher # => ProductFetcher
     def fetcher
       @fetcher ||= "#{name}Fetcher"
       @fetcher = @fetcher.constantize if @fetcher.is_a?(String)
