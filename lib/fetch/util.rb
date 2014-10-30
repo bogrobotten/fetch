22336e4527a4183dfdca4b7181c2fa256b761b41
Lasse Bunk <lasse@bunk.io>
2014-10-30 18:37:34 +0100

    Only rescue NameErrors concering the constantize

--- lib/fetch/util.rb
+++ lib/fetch/util.rb
@@ -30,7 +30,8 @@ module Fetch
     #   # => nil
     def self.safe_constantize(name)
       constantize(name)
-    rescue NameError
+    rescue NameError => e
+      raise e unless e.message.include?(name)
       nil
     end
   end
