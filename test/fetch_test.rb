edeec015bae610ef14472b4dcdf66a0d5435454a
Lasse Bunk <lasse@bunk.io>
2014-11-01 16:59:45 +0100

    Test fetch_if filter

--- test/fetch_test.rb
+++ test/fetch_test.rb
@@ -4,8 +4,29 @@ class FetchTest < Minitest::Test
   def test_basic_fetch
     fetchable = Fetchable.new
     MockFetcher(BasicModule).new(fetchable).fetch
-
     assert_equal %w{before fetch after}, fetchable.actions
+  end
+
+  def test_positive_fetch_if_filter
+    fetchable = Fetchable.new
+    mod = Class.new(BasicModule) do
+      fetch_if do
+        true
+      end
+    end
+    MockFetcher(mod).new(fetchable).fetch
+    assert_includes fetchable.actions, "fetch"
+  end
+
+  def test_negative_fetch_if_filter
+    fetchable = Fetchable.new
+    mod = Class.new(BasicModule) do
+      fetch_if do
+        false
+      end
+    end
+    MockFetcher(mod).new(fetchable).fetch
+    assert !fetchable.actions.include?("fetch")
   end
 
   class BasicModule < Fetch::Module
