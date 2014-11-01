6ce884e936a2e4eb62d2dabee3ef2e44cb8fdc5d
Lasse Bunk <lasse@bunk.io>
2014-11-01 16:43:35 +0100

    Begin simple testing

--- test/fetch_test.rb
+++ test/fetch_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
 class FetchTest < Minitest::Test
-  def setup
-    Fetch.configure do |config|
-      config.raise_on_error = true
+  def test_basic_fetch
+    fetchable = OpenStruct.new(callbacks: [])
+
+    mod = Class.new(Fetch::Module) do
+      def initialize(fetchable)
+        @fetchable = fetchable
+      end
+
+      before_fetch do
+        @fetchable.callbacks << "before"
+      end
+
+      fetch do
+        @fetchable.callbacks << "fetch"
+      end
+
+      after_fetch do
+        @fetchable.callbacks << "after"
+      end
     end
+
+    klass = Class.new(Fetch::Base) do
+      modules { mod }
+    end
+
+    klass.new(fetchable).fetch
+
+    assert_equal %w{before fetch after}, fetchable.callbacks
   end
 end
