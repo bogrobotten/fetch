9ef6bd5bc9b155e6d9388675bb9de2187689140d
Lasse Bunk <lasse@bunk.io>
2014-11-02 14:57:29 +0100

    Test callbacks

---
+++ test/callback_test.rb
@@ -0,0 +1,76 @@
+require "test_helper"
+
+class CallbackTest < Minitest::Test
+  def test_callbacks
+    actions = []
+    klass = Class.new do
+      include Fetch::Callbacks
+      define_callback :before, :after
+      before { actions << "something before" }
+      after { actions << "something after" }
+      def do_something
+        before
+        after
+      end
+    end
+    klass.new.do_something
+    assert_equal ["something before", "something after"], actions
+  end
+
+  def test_all_callbacks_are_run
+    actions = []
+    klass = Class.new do
+      include Fetch::Callbacks
+      define_callback :before
+      before { actions << "first" }
+      before { actions << "second" }
+      def do_something
+        before
+      end
+    end
+    klass.new.do_something
+    assert_equal ["first", "second"], actions
+  end
+
+  def test_value_from_last_callback_is_returned
+    klass = Class.new do
+      include Fetch::Callbacks
+      define_callback :before
+      before { "first" }
+      before { "second" }
+    end
+    assert_equal "second", klass.new.before
+  end
+
+  def test_callbacks_take_optional_arguments
+    actions = []
+    klass = Class.new do
+      include Fetch::Callbacks
+      define_callback :before, :after
+      before { |some_arg| actions << "one: #{some_arg}" }
+      after { |some_arg| actions << "two: #{some_arg.inspect}" }
+      def do_something
+        before("first")
+        after
+      end
+    end
+    klass.new.do_something
+    assert_equal ["one: first", "two: nil"], actions
+  end
+
+  def test_callbacks_are_inherited
+    before1 = Proc.new {}
+    before2 = Proc.new {}
+
+    superclass = Class.new do
+      include Fetch::Callbacks
+      define_callback :before
+
+      before(&before1)
+      before(&before2)
+    end
+
+    subclass = Class.new(superclass)
+    assert_equal [before1, before2], subclass.callbacks[:before]
+  end
+end
