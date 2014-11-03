a7de1e32e625dd368ed224c99d9e239c3b048998
Lasse Bunk <lasse@bunk.io>
2014-11-03 22:25:08 +0100

    Move request callbacks to defaults

--- lib/fetch/async.rb
+++ lib/fetch/async.rb
@@ -5,16 +5,19 @@ module Fetch
                            :defaults,
                            :before_process,
                            :after_process
+
+      base.defaults do |req|
+        req.before_process { before_process } if callback?(:before_process)
+        req.after_process { after_process } if callback?(:after_process)
+        req.failure { |code, url| failure(code, url) } if callback?(:failure)
+        req.error { |e| error(e) } if callback?(:error)
+      end
     end
 
     def requests
       self.class.callbacks[:request].map do |callback|
         Request.new.tap do |req|
           defaults(req)
-          req.before_process { before_process } if callback?(:before_process)
-          req.after_process { after_process } if callback?(:after_process)
-          req.failure { |code, url| failure(code, url) } if callback?(:failure)
-          req.error { |e| error(e) } if callback?(:error)
           instance_exec(req, &callback)
         end
       end.select(&:url)
